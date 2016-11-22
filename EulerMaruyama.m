cbade444986b44bff489d8645c32455571d7a42e
grantbaker <grant.baker@colorado.edu>
2016-11-21 19:16:14 -0700

    Numerical method complete, maybe

--- EulerMaruyama.m
+++ EulerMaruyama.m
@@ -1,8 +1,17 @@
-function [ sol ] = EulerMaruyama(system, t0, t1, N)
+function [ sol ] = EulerMaruyama(stochDE, X0, t1, N)
 %EULERMARUYAMA SDE solver
-%   Self-explanatory
+%   Self-explanator
 
+X = zeros(N,size(X0));
+X(1) = X0;
 
+dt = (t1-X0(1))/N;
+
+for ii=2:N
+    X(ii) = X(ii-1) + stochDE(X(ii-1),1)*dt + stochDE(X(ii-1),2)*normrnd(0,dt);
+end
+
+sol = X;
 
 
 end
