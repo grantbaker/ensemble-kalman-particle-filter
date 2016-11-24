d097c591d98cc70406feed8874296076d7dad36c
grantbaker <grant.baker@colorado.edu>
2016-11-23 23:21:50 -0700

    Sync

--- EnKF.m
+++ EnKF.m
@@ -1,3 +1,6 @@
 % Implementation of the Ensemble Kalman Filter for the
 % GBWB system. Generates the true solution using
 % Euler-Maruyama, and tracks it with an ensemble kalman filter
+
+trueSol = EulerMaruyama(@GBWB,[0,0.2,0,0],10,100000);
+
