ceb3687e82762a166e1a4d570e5626f3aa54277f
WillBarham <WillBarham@users.noreply.github.com>
2016-12-02 16:55:16 -0700

    Fixed Bugs

--- PF_SDE_Driver.m
+++ PF_SDE_Driver.m
@@ -4,21 +4,20 @@ clc
 
 % Constants associated with the filter
     varNum = 3;
-
-% Initialize the particles and weights
-    NP = 300; % Number of particles
-    wPF = ones(1,NP)/NP;
-    wPF_store = zeros(NP, 200);
-    vPF = randn(varNum, NP);
-    vPF_store = randn(varNum, NP, 200);
-
-% Set values for true solution
     deltaT = 10^-4;
     % adding extra deltaT since we don't want to observe system at time = 0 
     tFin = 10 + deltaT;
     numObs = 200;
+    numberOfSteps = (tFin - deltaT)/deltaT;
     stepsBetweenObs = (tFin - deltaT)/(deltaT*numObs);
     InitialCond = randn(varNum,1);
+    
+% Initialize the particles and weights
+    NP = 300; % Number of particles
+    wPF = ones(1,NP)/NP;
+    wPF_store = zeros(NP, numberOfSteps);
+    vPF = randn(varNum, NP);
+    vPF_store = randn(varNum, NP, numberOfSteps);
 
 % Obtain trajoctory of system
     [T, XT] = SDESolver(deltaT, 1, tFin, InitialCond);
@@ -33,38 +32,42 @@ clc
     %H = tmp;
     R = (1/10)*eye(size(H,1));
     % Obtain observations
-    obs = H*XTObserved + sigma0*randn(size(H,1),200);
+    obs = H*XTObserved + sigma0*randn(size(H,1),numObs);
     
 for ii = 1:200
     disp(ii)
     % PF
     % forecast particles
     [~,sol] = SDESolver(deltaT, NP, stepsBetweenObs*deltaT, vPF');
+    vPF_store(:,:,((ii-1)*stepsBetweenObs+1):(ii*stepsBetweenObs)) = ...
+                permute(sol(:,:,:), [3,2,1]);
     vPF = squeeze(sol(end,:,:))';
     
     for jj = 1:NP
         wPF(jj) = exp(-.5*(norm(obs(:,ii) - H*vPF(:,jj),2)/sigma0)^2);
     end
     wPF = wPF/sum(wPF);
-    wPF_store(:,ii) = wPF;
+    wPF_store(:,((ii-1)*stepsBetweenObs+1):(ii*stepsBetweenObs)) = ...
+                    repmat(wPF,500,1)';
 
     % Resample
     NN = randsample(NP,NP,true,wPF);
     vPF = vPF(:,NN);
-    vPF_store(:,:,ii) = vPF;
+    vPF_store(:,:,ii*stepsBetweenObs) = vPF;
 end
 
 % Compute the mean at each time
-vPF_m = zeros(varNum, 200);
+vPF_m = zeros(varNum, numberOfSteps);
 for ii = 1:varNum
    vPF_m(ii, :) = sum(squeeze(vPF_store(ii,:,:)).*wPF_store,1);
 end
 
-RMS = sqrt(mean((vPF_m - XTObserved).^2, 1));
-RMS_unobserved = sqrt(mean((XTObserved(1,:) - vPF_m(1,:)).^2,1));
+RMS = sqrt(mean((vPF_m - XT).^2, 1));
+RMS_unobserved = sqrt(mean((XT(1,:) - vPF_m(1,:)).^2,1));
 
 figure
 hold on
-plot(vPF_m(1,:))
-plot(XTObserved(1,:))
+plot(T, vPF_m(1,:))
+plot(T, XT(1,:))
+legend('PF', 'True')
 hold off
--- SDE_EnKPF_Driver.m
+++ SDE_EnKPF_Driver.m
@@ -4,59 +4,62 @@ clc
 
 global H R gamma;
 
-gamma = 0.7;
+gamma = 0.95;
 
 % Constants associated with the filter
-ensNum = 100;
-varNum = 3;
-
-% Set values for true solution
-deltaT = 10^-4;
-tFin = 10 + deltaT; % adding extra deltaT since we don't want to observe system at time = 0
-numObs = 200;
-stepsBetweenObs = (tFin - deltaT)/(deltaT*numObs);
-InitialCond = randn(varNum,1);
+    ensNum = 30;
+    varNum = 3;
+    deltaT = 10^-4;
+    tFin = 10 + deltaT; % adding extra deltaT since we don't want to observe system at time = 0
+    numObs = 200;
+    numberOfSteps = (tFin - deltaT)/deltaT;
+    stepsBetweenObs = (tFin - deltaT)/(deltaT*numObs);
+    InitialCond = randn(varNum,1);
 
 % Obtain trajoctory of system
-[T, XT] = SDESolver(deltaT, 1, tFin, InitialCond);
-XT = XT(2:end,:)';T = T(2:end);
+    [T, XT] = SDESolver(deltaT, 1, tFin, InitialCond);
+    XT = XT(2:end,:)';T = T(2:end);
 
-XTObserved = XT(:,1:stepsBetweenObs:end);
+    TObserved = T(:,1:stepsBetweenObs:end);
+    XTObserved = XT(:,1:stepsBetweenObs:end);
 
 % Initialize observation matrix and obs err cov mat
-sigma0 = sqrt(1/10);
-tmp = eye(varNum);
-H = tmp(2:3,:);
-%H = tmp;
-R = (1/10)*eye(size(H,1));
-% Obtain observations
-obs = H*XTObserved + sigma0*randn(size(H,1),200);
+    sigma0 = sqrt(1/5);
+    tmp = eye(varNum);
+    H = tmp(2:3,:);
+    %H = tmp;
+    R = (1/10)*eye(size(H,1));
+    % Obtain observations
+    obs = H*XTObserved + sigma0*randn(size(H,1),numObs);
 
-% Initialize ensemble with random numbers
-EnKPF = randn(varNum,ensNum);
+% Initialize ensemble
+    EnKPF = bsxfun(@plus, XTObserved(:,1), sigma0*randn(varNum, ensNum));
 % Store the results
-EnKPF_store = zeros(varNum, ensNum, 200);
+    EnKPF_store = zeros(varNum, ensNum, numberOfSteps);
 
 for ii = 1:numObs
     disp(ii)
     % EnKPF
     % forecast ensemble
     [~,sol] = SDESolver(deltaT, ensNum, stepsBetweenObs*deltaT, EnKPF');
-    EnKPF_store(:,:,ii) = squeeze(sol(end,:,:))';
+    EnKPF_store(:,:,((ii-1)*stepsBetweenObs+1):(ii*stepsBetweenObs)) = ...
+                permute(sol(:,:,:), [3,2,1]);
+    EnKPF = squeeze(sol(end,:,:))';
     
     % analysis update
-    update = EnKPF_update(EnKPF_store(:,:,ii), obs(:,ii), varNum, ensNum);
+    update = EnKPF_update(EnKPF, obs(:,ii), varNum, ensNum);
                         
     EnKPF = update;
-    EnKPF_store(:,:,ii) = EnKPF;
+    EnKPF_store(:,:,ii*stepsBetweenObs) = EnKPF;
 end
 
 mean_EnKPF = squeeze(mean(EnKPF_store,2));
-RMS = sqrt(mean((XTObserved-mean_EnKPF).^2,1));
-RMS_unobserved = sqrt(mean((XTObserved(1,:) - mean_EnKPF(1,:)).^2,1));
+RMS = sqrt(mean((XT - mean_EnKPF).^2,1));
+RMS_unobserved = sqrt(mean((XT(1,:) - mean_EnKPF(1,:)).^2,1));
 
 figure
 hold on
-plot(linspace(0,tFin+deltaT,mean_EnKPF(1,:))
-plot(XTObserved(1,:))
+plot(T, mean_EnKPF(1,:))
+plot(T, XT(1,:))
+legend('EnKPF', 'True')
 hold off
